9c74835dc30c33ae3d105d24a20a3591eb01968b
hasslu-shun <97440434+hasslu-shun@users.noreply.github.com>
2022-03-23 16:32:23 +0900

    Delete hasslu-media-main directory

--- hasslu-media-main/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  // reactStrictMode: false,
-  // target: 'serverless',
-  images: {
-    domains: ['images.ctfassets.net'],
-},
-}
